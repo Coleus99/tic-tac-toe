d80dc95c8d37d67c67f52b97f27bf663ec061e3f
coleus99 <christopherjohncoles@gmail.com>
2020-10-15 12:25:55 +0100

    failed attempt at using an recursive function to determine winner i feel like this should be possible, but i have wasted too much time on this

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ Only need to expose the Gameboard playmove function for players to use, don't wa
 allow access to squares array
 */
 const Gameboard = (() => {
-    const squares = [['x','o','x'],['o','x','o'],['x','o','x']];
+    const squares = [['a','b','x'],['d','x','f'],['x','h','i']];
     const boardOuter = document.querySelector('.board');
     function createBoard(){
         for (let i=0; i<3; i++){
@@ -18,6 +18,7 @@ const Gameboard = (() => {
                 columnDiv.setAttribute('row',i);
                 columnDiv.setAttribute('column',j);
                 rowDiv.appendChild(columnDiv);
+                console.log(checkSquare(i,j,squares[i][j]));
             };
             boardOuter.appendChild(rowDiv);
         }
@@ -25,9 +26,54 @@ const Gameboard = (() => {
     function playMove(row, column, symbol){
         // Need to flesh out this function with user input from the player function
         createBoard();
+        console.log(checkWin());
     };
+    function checkSquare(row,col,value){
+        if (
+            value===squares[row][col+1] && value===squares[row][col+2] ||
+            value===squares[row+1][col] && value===squares[row+2][col]
+            ){
+            return value;
+        }
+        else {
+            return null;
+        }
+            // value===squares[row+1][col]) && value===squares[row+2][col] ||
+            // value===squares[row+1][col+1] && value===squares[row+2][col+2] ||
+            // value===squares[row-1][col-1] && value===squares[row-2][col-2]
+    }
+    function checkWin(){
+        // debugger;
+        
+
+        // function find(row,col,history){
+        //     if (history===target){
+        //         return 'win'
+        //     }
+        //     // if(history.length>1 && history[0]!=history[1]){
+        //     //     return null;
+        //     // }
+        //     else if (row>1||col>1){
+        //         return null;
+        //     }
+        //     else {
+        //         return find(row+1,col,(history + squares[row+1][col])) ||
+        //         find(row,col+1,(history + squares[row][col+1])) ||
+        //         find(row+1,col+1,(history + squares[row+1][col+1])) ;
+        //     }
+        // }
+        // return find (0,0,squares[0][0]);
+    }
+        /* 
+        if cell is empty run checkwin(row+1,col) || checkwin(row,col+1)
+        if cell is not empty store value
+            if stored value = xxx || ooo return 'win'
+            if adjacent cell is same store value
+                repeat
+        */
     return {
-        playMove
+        playMove,
+        squares
     }
 })();
 
